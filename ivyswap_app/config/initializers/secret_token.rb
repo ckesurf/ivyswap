93fc355347d3d1a17c557fd7da6112c2d80ef5f1
Chris Erlendson <cke2106@columbia.edu>
2013-01-26 15:52:05 -0500

    removed redundant ivyswap_app directory

--- ivyswap_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-IvyswapApp::Application.config.secret_token = 'b5a806e58653aa5ac23851e6626ee51da1e0b5cc10e07a83310da2ba4e258adc94ee8fb2a0df8788701da2a1f50bf0c412846e0b35e00eee185e7afd48411ae2'
