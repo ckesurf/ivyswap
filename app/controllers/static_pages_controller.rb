eff7773ce6ca4653e50bbf96fc608353a0ebef89
Ben Nappier <ben2113@columbia.edu>
2013-02-07 18:28:30 -0600

    lists every users product on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @product  = current_user.products.build
+      @products = Product.all
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
