ec4b2bb33adee3048ecb903b350f172ad2312b9c
Chris Erlendson <cke2106@columbia.edu>
2013-02-09 09:48:25 -0500

    added app/mailers/notifier.rb and test

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,10 @@
+class Notifier < ActionMailer::Base
+  default :from => "ckesurf@gmail.com"
+
+  # send a signup email to the user, pass in the user object that contains the user's email address
+  def signup_email(user)
+  	@user = user
+    mail( :to => user.email,
+    :subject => "Thanks for signing up" )
+  end
+end
