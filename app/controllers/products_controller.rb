22df79ee10ca6b652384ddda5b2f945ab7002576
Ben Nappier <ben2113@columbia.edu>
2013-02-08 20:41:19 -0600

    fixed flash message

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -49,7 +49,7 @@ class ProductsController < ApplicationController
       if @product.save
         #format.html { redirect_to @product, :notice => 'Product was successfully created.' }
         #format.json { render :json => @product, :status => :created, :location => @product }
-        flash[:success] = "Micropost created!"
+        flash[:success] = "You posted #{@product.title}!"
         redirect_to root_url
       else
         @feed_items = []
